b4151d81b0ce65abf84691edb9d2ad00d0896365
germfy <gdominguezmontes@gmail.com>
2016-07-29 18:01:18 -0500

    Adaptación de acuerdo con documentación de Bluemix

--- app.js
+++ app.js
@@ -13,23 +13,25 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
 //var wCredentials = vcap_services.weatherinsights[0].credentials;
-//var wCredentialsHost = appEnv.services["weatherinsights"]? appEnv.services["weatherinsights"][0].credentials.url : "";
-var wHost = appEnv.services["weatherinsights"][0].credentials.host;
+var wCredentialsHost = appEnv.services["weatherinsights"]? appEnv.services["weatherinsights"][0].credentials.url : "";
+/*var wHost = appEnv.services["weatherinsights"][0].credentials.host;
 var wUsername = appEnv.services["weatherinsights"][0].credentials.username;
-var wPassword = appEnv.services["weatherinsights"][0].credentials.password;
+var wPassword = appEnv.services["weatherinsights"][0].credentials.password;*/
 
 app.get('/weather',function(req,res) {
   console.log(wHost +" "+ wUsername +" "+ wPassword);
   var queryStr = url.parse(req.url,true).query;
-  //var urlweather = wCredentialsHost + '/api/weather/v1/geocode/'+queryStr.lat+'/'+queryStr.lon+'/observations.json?language=es-MX&units=m'
+  var urlweather = wCredentialsHost + '/api/weather/v1/geocode/'+queryStr.lat+'/'+queryStr.lon+'/observations.json?language=es-MX&units=m'
+  console.log(urlweather);
   var optionsgetmsg = {
-    host : wHost,
+    url : urlweather,
+    /*host : wHost,
     path : '/api/weather/v1/geocode/'+queryStr.lat+'/'+queryStr.lon+'/observations.json?language=es-MX&units=m',
     port : 443,
     protocol: 'https:',
     auth : wUsername + ':' + wPassword,
     //path: '/v1/geocode/'+queryStr.lat+'/'+queryStr.lon+'/observations.json',
-    //path : '/api/weather/v2/observations/current?units=m&language=es-MX&geocode='+ queryStr.latlon,
+    //path : '/api/weather/v2/observations/current?units=m&language=es-MX&geocode='+ queryStr.latlon,*/
     method : 'GET',
     headers : {
       "Content-Type": "application/json;charset=utf-8",
